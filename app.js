8bebcb0e49e44f897657789508c7e82e7b8872f7
Toshihiro Shimizu <shimizu.toshihiro@gmail.com>
2011-08-27 14:57:51 +0900

    Twitter OAuth

--- app.js
+++ app.js
@@ -6,10 +6,13 @@
 var express = require('express'),
     nko = require('nko')('sWq0rm8zUcxb3Isa'),
     RedisStore = require('connect-redis')(express),
-    scraper = require('./lib/scraper');
+    auth = require('connect-auth'),
+    scraper = require('./lib/scraper'),
+    settings = require('./settings.js'),
+    User = require('./lib/user.js');
 
 var app = module.exports = express.createServer();
-
+var port;
 // Configuration
 
 app.configure(function(){
@@ -22,24 +25,75 @@ app.configure(function(){
     secret: 'mesolabs',
     store: new RedisStore()
   }));
-  app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
+  app.use(auth([auth.Twitter({
+    consumerKey: settings.consumer_key,
+    consumerSecret: settings.consumer_secret,
+    callback: 'http://localhost:3000/signin'
+  })]));
+  app.use(app.router);
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  port = 3000;
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+  app.use(auth[auth.Twitter({
+    consumerKey: settings.consumer_key,
+    consumerSecret: settings.consumer_secret,
+    callback: 'http://mesolabs.no.de/signin'
+  })]);
+  app.use(app.router);
+  app.use(express.errorHandler());
+  port = 80;
 });
 
 // Routes
+const TITLE = 'Stalkr';
 
 app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Express'
-  });
+  if (!req.isAuthenticated()) {
+    res.render('index', {
+      title: TITLE,
+      authenticated: false
+    });
+  } else {
+    var user = new User(
+      req.getAuthDetails().user.user_id,
+      req.getAuthDetails().user.username,
+      req.getAuthDetails()['twitter_oauth_token'],
+      req.getAuthDetails()['twitter_oauth_token_secret']
+    );
+    console.dir(user);
+    res.render('authok', {
+      title: TITLE,
+      authenticated: true,
+      name: user.name
+    });
+  }
+});
+
+app.get('/signin', function(req, res) {
+  if (req.query.denied) {
+    res.render('authng', {
+      title: TITLE,
+      authenticated: false
+    });
+  } else {
+    req.authenticate(['twitter'], function(err, authenticated) {
+      if (err) return console.log(err);
+      if (authenticated) {
+        res.redirect('/', 303);
+      }
+    });
+  }
+});
+
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/', 303);
 });
 
 app.post('/', function(req, res) {
@@ -53,5 +107,5 @@ app.post('/', function(req, res) {
   res.send(200);
 });
 
-app.listen(3000);
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
---
+++ settings.js
@@ -0,0 +1,6 @@
+module.exports = {
+  'consumer_key'       : 'X2ZXssNWxvQpZVU4zppSoQ',
+  'consumer_secret'    : 'GyrbbQiJnnR5X3J645aAM4jRaZ64x0qBUzED0vh4',
+  'access_token'       : 'YOUR ACCESS TOKEN HERE',
+  'access_token_secret': 'YOUR ACCESS TOKEN SECRET HERE'
+}
